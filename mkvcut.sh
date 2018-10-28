27a6dd1f5b209d87e0b32bc8834da0dcd8a63002
Hello Party <1559567+hello-party@users.noreply.github.com>
2018-10-28 01:31:07 -0700

    Extra quotes to fix whitespace issue

--- mkvcut.sh
+++ mkvcut.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 file="$1"
-ffmpeg -i $1 -ss $2 -t $3 -c:v libx264 -crf 22 -c:a aac -movflags faststart ""${file%.*}"-output.$4"
+ffmpeg -i $1 -ss $2 -t $3 -c:v libx264 -crf 22 -c:a aac -movflags faststart "${file%.*}"-output.$4
